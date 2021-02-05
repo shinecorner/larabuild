d27ab09f0bad1d01fb0c7124ecbe2aa05e3197a6
Mandip Malviya <mandipmalaviya@gmail.com>
2021-02-05 11:39:48 +0530

    change

--- reference/app/Http/Controllers/TestController.php
+++ reference/app/Http/Controllers/TestController.php
@@ -7,10 +7,10 @@ use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
-    public function qbuilder(){
+    public function qbuilder(Request $request){
 
         DB::enableQueryLog();
-        //        $array = [
+//                $array = [
 //            ['name' => 'Phillips Lamp', 'price' => 150],
 //            ['name' => 'Nova lamp', 'price' => 90],
 //            ['name' => 'Samsung lamp', 'price' => 70]
@@ -127,7 +127,243 @@ class TestController extends Controller
 //        }
 //        $users = DB::table('users')->select('name')->distinct()->get();
 //        print_r($users);
-        dd(DB::getQueryLog());
+
+//        $orders = DB::table('orders')
+//            ->selectRaw('price * ? as price_with_tax', [1.0825])
+//            ->get();
+//        dd($orders);
+
+//        $orders = DB::table('orders')
+//            ->whereRaw('price > IF(state = "Maharashtra", ?, 400)', [1200])
+//            ->get();
+//        dd($orders);
+
+//        $orders = DB::table('orders')
+//            ->select('department', DB::raw('SUM(price) as total_sales'))
+//            ->groupBy('department')
+//            ->havingRaw('total_sales > ?', [1500])
+//            ->get();
+//        dd($orders);
+//        $orders = DB::table('orders')
+//            ->select('*')
+//            ->addSelect(DB::raw('(updated_at - created_at) as duration'))
+//            ->orderByRaw('updated_at - created_at DESC')
+//            ->get();
+//        dd($orders);
+
+//        $orders = DB::table('orders')
+//            ->select('city', 'state','department')
+//            ->groupByRaw('state, city, department')
+//            ->get();
+//        dd($orders);
+
+//        $users = DB::table('users')
+//            ->join('contacts', 'users.id', '=', 'contacts.user_id')
+//            ->join('orders', 'users.id', '=', 'orders.user_id')
+//            ->select('users.*', 'contacts.phone', 'orders.price')
+//            ->get();
+//        dd($users);
+
+//        $users = DB::table('users')
+//            ->leftJoin('post', 'users.id', '=', 'post.user_id')
+//            ->get();
+//        dd($users);
+
+//        $sizes = DB::table('size')
+//            ->crossJoin('color')
+//            ->get();
+//        dd($sizes);
+
+//        $users = DB::table('users')
+//            ->join('contacts', function ($join) {
+//                $join->on('users.id', '=', 'contacts.user_id')
+//                    ->where([
+//                            ['contacts.user_id', '>', 2],
+//                            ['users.salary', '>=', 5500],
+//                        ])
+//                ;
+//            })
+////            ->join('contacts','users.id', '=', 'contacts.user_id')
+////            ->where('contacts.user_id','>', 3)
+//            ->get();
+//        dd($users);
+
+//        $latestPosts = DB::table('post')
+//            ->select('user_id', DB::raw('MAX(created_at) as last_post_created_at'))
+//            ->groupBy('user_id');
+//
+//        $users = DB::table('users')
+//            ->joinSub($latestPosts, 'latest_posts', function ($join) {
+//                $join->on('users.id', '=', 'latest_posts.user_id');
+//            })->get();
+
+//        select * from `users` inner join (select `user_id`, MAX(created_at) as last_post_created_at from `post` group by `user_id`) as `latest_posts` on `users`.`id` = `latest_posts`.`user_id`
+
+//        $users = DB::table('users')
+//            ->where('designation', '=', 'designer')
+//            ->orWhere('salary', '>=', 8000)
+//            ->get();
+//        dd($users);
+
+//        $users = DB::table('users')
+//            ->where('designation', '=', 'designer')
+//            ->orWhere(function($query) {
+//                $query->where('designation', 'programmer')
+//                    ->where('salary', '>=', 8000);
+//            })
+//            ->get();
+//        dd($users);
+
+//        $users = DB::table('users')
+//            ->whereBetween('salary', [5000, 7000])
+//            ->get();
+//        dd($users);
+
+//        $users = DB::table('users')
+//            ->whereIn('id', [4, 7, 9])
+//            ->get();
+//        dd($users);
+
+//        $users = DB::table('users')
+//            ->whereNull('email_verified_at')
+//            ->first();
+//        dd($users);
+
+//        $users = DB::table('users')
+//            ->whereDate('created_at', '2021-01-25')
+//            ->get();
+//        dd($users);
+
+//        $users = DB::table('users')
+//        ->whereMonth('created_at', '02')
+//        ->get();
+//        dd($users);
+
+//        $users = DB::table('users')
+//            ->where('created_at', '=', '11:20:45')
+//            ->get();
+//        dd($users);
+
+//        $users = DB::table('users')
+//            ->whereColumn('created_at', 'updated_at')
+//            ->get();
+//        dd($users);
+
+//        $users = DB::table('users')
+//            ->whereColumn([
+//                ['first_name', '=', 'last_name'],
+//                ['updated_at', '>', 'created_at'],
+//            ])->get();
+
+//        $users = DB::table('users')
+//            ->where('designation', '=', 'programmer')
+//            ->where(function ($query) {
+//                $query->where('salary', '>', 7000)
+//                    ->orWhere('name', '=', 'Jigar');
+//            })
+//            ->get();
+//        dd($users);
+//        select * from users where exists ( select 1 from orders where orders.user_id = users.id )
+
+//        dd(DB::getQueryLog());
+
+        // $users = DB::table('users')->where(function ($query) {
+        // $query->select('type')
+        //     ->from('membership')
+        //     ->whereColumn('membership.user_id', 'users.id')
+        //     ->orderByDesc('membership.start_date')
+        //     ->limit(1);
+        // }, 'VIP')->get();
+        // dd($users);
+
+
+        // $orders = DB::table('orders')->where('price', '<', function ($query) {
+        //         $query->selectRaw('avg(o.price)')->from('orders as o');
+        //     })->get();
+        // dd($orders);
+
+        // $users = DB::table('users')
+        //         ->orderBy('name', 'desc')
+        //         ->orderBy('email', 'asc')
+        //         ->get();
+
+        // $user = DB::table('users')
+        //         ->latest()
+        //         ->first();
+
+        // $randomUser = DB::table('users')
+        //         ->inRandomOrder()
+        //         ->first();
+
+        // dd($randomUser);
+
+        // $query = DB::table('users')->orderBy('name');
+
+        // $usersOrderedByEmail = $query->reorder('email', 'desc')->get();
+
+        // $users = DB::table('users')
+        //         ->groupBy('name')
+        //         ->having('name', '=', 'Jigar')
+        //         ->get();
+        // dd($users);
+
+        // $users = DB::table('users')
+        //         ->groupBy('name', 'salary')
+        //         // ->having('account_id', '>', 100)
+        //         ->get();
+        //         dd($users);
+
+        // $per_page_record = 2;
+        // $current_page = 2;
+
+        // $users = DB::table('users')->skip(($current_page - 1) * $per_page_record)->take($per_page_record)->get();
+        // dd($users);
+
+
+        $designation = $request->input('designation');
+
+        // $users = DB::table('users')
+        //         ->when($designation, function ($query, $designation) {
+        //             return $query->where('designation', $designation);
+        //         })
+        //         ->get();
+        //  dd($users);   
+        
+        // $sortBy = $request->input('sort_by');
+
+        // $users = DB::table('users')
+        //         ->when($sortBy, function ($query, $sortBy) {
+        //             return $query->orderBy($sortBy);
+        //         }, function ($query) {
+        //             return $query->orderBy('name');
+        //         })
+        //         ->get();
+        // dd($users);
+
+        // $id = DB::table('size')->insertGetId(
+        //         ['cloth_size' => 'XXL']
+        //     );
+            
+        // DB::table('users')->upsert([
+        //         ['name' => 'Jigar', 'designation' => 'designer', 'salary' => 9990], 
+        //     ], ['name', 'designation'], ['salary']);
+
+        // DB::table('users')
+        // ->updateOrInsert(
+        //         ['name' => 'Jigar', 'designation' => 'designer'],
+        //         ['salary' => '7500']
+        // );
+        
+        // $affected = DB::table('orders')
+        //       ->where('id', 1)
+        //       ->update(['detail->price' => 200]);
+
+        // DB::table('items')->truncate();
+        // DB::table('users')->get()->dd();
+        // DB::table('users')->get()->dump();
+        // echo '1';
+        // dd(DB::getQueryLog());
+
     }
 
 }
---
+++ reference/database/migrations/2021_02_05_040429_create_membership_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembershipTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('membership', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->date('start_date')->nullable();
+            $table->string('type')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('membership');
+    }
+}
